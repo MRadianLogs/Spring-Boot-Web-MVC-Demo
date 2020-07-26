1cfeb605eb466214d0bbdb7d3323a69aaa7f4de9
Reyes, Michael <mradianlogs@gmail.com>
2020-07-26 15:00:50 -0500

    Initial Commit.

---
+++ Web-MVC-content/src/test/java/com/example/Web/MVC/content/WebMvcContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Web.MVC.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
